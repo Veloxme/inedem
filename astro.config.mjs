c6311895e6b9bd2ca4ae6d4bd4ea069a69322de6
José Manuel Grande Elenes <veloxme@gmail.com>
2025-11-11 12:31:24 -0700

    github accions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Veloxme.github.io",
+  base: "/inedem/",
   vite: {
     plugins: [tailwindcss()],
   },
